1cad8cfc2cc363d0500e96a875058a0dcb3d9461
arha06001 <arha06001@E-PW0234JA>
2023-09-20 09:49:12 +0200

    Add project files.

---
+++ Uppgift 2.3 inlämningsuppgift/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Atte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int startavgift = 300;
+
+            
+            int kostnadPerKilometer = 1;
+
+            
+            int kostnadPerExtraDag = 500;
+
+            
+            Console.Write("Hur många dagar vill du hyra bilen? ");
+            int antalDagar = int.Parse(Console.ReadLine());
+
+            
+            Console.Write("Hur många kilometer ska du köra? ");
+            double antalKilometer = double.Parse(Console.ReadLine());
+
+            
+            int totalHyra = startavgift + (antalDagar - 1) * kostnadPerExtraDag + (int)(antalKilometer * kostnadPerKilometer);
+
+            
+            Console.WriteLine($"Den totala hyran blir: {totalHyra} kr");
+        }
+    }
+}
